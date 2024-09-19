6339635757feb484743c2332d8166f62eb1564a1
Dominique Guerrero <domguerr@hotmail.com>
2024-09-19 16:03:08 -0600

    User Auth Middlewares

--- backend/utils/auth.js
+++ backend/utils/auth.js
@@ -64,3 +64,22 @@ const restoreUser = (req, res, next) => {
     return next();
   });
 };
+
+// backend/utils/auth.js
+// ...
+
+// If there is no current user, return an error
+const requireAuth = function (req, _res, next) {
+  if (req.user) return next();
+
+  const err = new Error('Authentication required');
+  err.title = 'Authentication required';
+  err.errors = { message: 'Authentication required' };
+  err.status = 401;
+  return next(err);
+}
+
+// backend/utils/auth.js
+// ...
+
+module.exports = { setTokenCookie, restoreUser, requireAuth };
