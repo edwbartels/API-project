4fde4c680966c74b13e94d922fc73bb7cd5ffea1
Dominique Guerrero <domguerr@hotmail.com>
2024-09-19 16:16:28 -0600

    Add User logout backend endpoint

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -43,4 +43,16 @@ router.post(
   }
 );
 
+// Log out
+router.delete(
+  '/',
+  (_req, res) => {
+    res.clearCookie('token');
+    return res.json({ message: 'success' });
+  }
+);
+
+
+
+
 module.exports = router;
