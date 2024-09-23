4d40492b4f85e01f9e9184d1b591dcd0a75d0292
edwbartels <edwbartels@gmail.com>
2024-09-23 16:45:58 -0400

    get by id, get all by current user, create

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -2,45 +2,108 @@ const express = require('express');
 const router = express.Router();
 
 const { Spot, Review, Booking, SpotImage } = require('../../db/models');
-const { Op } = require('sequelize');
+const { Op, fn, col } = require('sequelize');
+const { requireAuth } = require('../../utils/auth');
 
+// GET all spots
 router.get('/', async (req, res, next) => {
 	let spots = [];
 	let avgRating;
 	spots = await Spot.findAll({
-		include: [
-			{
-				model: Review,
-				attributes: ['stars'],
-				// through: {
-				// 	attributes: [],
-				// },
-			},
-			{
-				model: SpotImage,
-				attributes: ['url', 'preview'],
-				// where: {
-				// 	preview: true,
-				// },
-			},
-		],
+		// include: [
+		// 	{
+		// 		model: Review,
+		// 		attributes: ['stars'],
+		// 		// through: {
+		// 		// 	attributes: [],
+		// 		// },
+		// 	},
+		// 	{
+		// 		model: SpotImage,
+		// 		attributes: ['url', 'preview'],
+		// 		// where: {
+		// 		// 	preview: true,
+		// 		// },
+		// 	},
+		// ],
 	});
 	spots.forEach((el) => {
-		if (el.Reviews.length) {
-			let sum = el.Reviews.reduce((acc, cur) => acc + cur) / el.Reviews.length;
-			el.avgRating = sum;
-		}
-		if (el.SpotImages.length) {
-			el.previewImage = SpotImages.findOne({
-				where: {
-					spotId: el.id,
-					preview: true,
-				},
-			}).url;
-		}
+		// if (el.Reviews.length) {
+		// 	let avg = el.Reviews.reduce((acc, cur) => acc + cur) / el.Reviews.length;
+		// 	el.avgRating = sum;
+		// }
+		const { avg } = Review.findOne({
+			where: {
+				spotId: el.id,
+			},
+			attributes: [[fn('AVG', col('stars')), 'avgRating']],
+			raw: true,
+		});
+		el.avgRating = avg ? parseFloat(avg) : 0;
+		const { imgUrl } = SpotImage.findOne({
+			where: {
+				spotId: el.id,
+				preview: true,
+			},
+			attributes: ['url'],
+			raw: true,
+		});
+		el.previewImage = imgUrl ?? null;
 	});
 
 	res.json(spots);
+});
+
+// GET all spots owned by current user
+
+router.get('/current', requireAuth, async (req, res, next) => {
+	const { user } = req;
+	const spots = await Spot.findAll({
+		where: { ownerId: user.id },
+	});
+
+	res.status(200).json(spots);
+});
+
+// GET spot by id
+
+router.get('/:spotId', async (req, res, next) => {
+	const spot = await Spot.findByPk(req.params.spotId);
+	const { count, avg } = await Review.findAndCountAll({
+		where: {
+			spotId: spot.id,
+		},
+		attributes: [[fn('AVG', col('stars')), 'avgRating']],
+	});
+	spot.numReviews = count;
+	spot.avgStarRating = avg ? parseFloat(avg) : 0;
+	spot.SpotImages = SpotImage.findAll({
+		where: {
+			spotId: spot.id,
+		},
+		attributes: ['id', 'url', 'preview'],
+	});
+	res.status(200).json(spot);
+});
+
+router.post('/', requireAuth, async (req, res, next) => {
+	const { user } = req;
+	console.log(user);
+	const { address, city, state, country, lat, lng, name, description, price } =
+		req.body;
+	const spot = await Spot.create({
+		ownerId: user.id,
+		address: address,
+		city: city,
+		state: state,
+		country: country,
+		lat: lat,
+		lng: lng,
+		name: name,
+		description: description,
+		price: price,
+	});
+	res.status(201).json(spot);
 });
 
 module.exports = router;
