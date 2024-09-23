31da7e642b6a9a13b6c4b5beeb04a788f2341eb8
edwbartels <edwbartels@gmail.com>
2024-09-23 15:24:38 -0400

    spots seeder/route + get all spots (preliminary)

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,9 +1,46 @@
 const express = require('express');
-const { Op } = require('sequelize');
-const { Spot } = require('../../db/models');
 const router = express.Router();
 
+const { Spot, Review, Booking, SpotImage } = require('../../db/models');
+const { Op } = require('sequelize');
+
 router.get('/', async (req, res, next) => {
-	const spots = await Spot.findAll({});
-	return res.status(200).json(spots);
+	let spots = [];
+	let avgRating;
+	spots = await Spot.findAll({
+		include: [
+			{
+				model: Review,
+				attributes: ['stars'],
+				// through: {
+				// 	attributes: [],
+				// },
+			},
+			{
+				model: SpotImage,
+				attributes: ['url', 'preview'],
+				// where: {
+				// 	preview: true,
+				// },
+			},
+		],
+	});
+	spots.forEach((el) => {
+		if (el.Reviews.length) {
+			let sum = el.Reviews.reduce((acc, cur) => acc + cur) / el.Reviews.length;
+			el.avgRating = sum;
+		}
+		if (el.SpotImages.length) {
+			el.previewImage = SpotImages.findOne({
+				where: {
+					spotId: el.id,
+					preview: true,
+				},
+			}).url;
+		}
+	});
+
+	res.json(spots);
 });
+
+module.exports = router;
