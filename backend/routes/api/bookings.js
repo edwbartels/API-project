26848e22cfac2d4d1fe8d03ffbd7d8db5e9efbb8
edwbartels <edwbartels@gmail.com>
2024-09-23 18:12:16 -0400

    get bookings by current user

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -5,4 +5,14 @@ const { Spot, Review, Booking, SpotImage } = require('../../db/models');
 const { Op, fn, col } = require('sequelize');
 const { requireAuth } = require('../../utils/auth');
 
+// GET bookings by current user
+
+router.get('/current', requireAuth, async (req, res, next) => {
+	const { user } = req;
+	const bookings = await Booking.findAll({
+		where: { ownerId: user.id },
+	});
+	res.status(200).json(bookings);
+});
+
 module.exports = router;
