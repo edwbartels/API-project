b073119a2e296ef8dc7c256df2c1af21371a15ac
edwbartels <edwbartels@gmail.com>
2024-09-23 18:31:58 -0400

    spot-images router

---
+++ backend/routes/api/spot-images.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+const { Spot, Review, Booking, SpotImage } = require('../../db/models');
+const { Op, fn, col } = require('sequelize');
+const { requireAuth } = require('../../utils/auth');
+
+module.exports = router;
