dfa5b72b2595f4642030b2a5977edab108f36d60
edwbartels <edwbartels@gmail.com>
2024-09-24 14:38:14 -0400

    error handling completed 1st run all reviews

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -46,18 +46,30 @@ router.get('/current', requireAuth, async (req, res, next) => {
 router.post('/:reviewId/images', requireAuth, async (req, res, next) => {
 	const { user } = req;
 	const { url } = req.body;
-	const review = await Review.findByPk(req.params.reviewId);
-	if (!review)
+	const review = await Review.findByPk(req.params.reviewId, {
+		include: {
+			model: ReviewImage,
+			attributes: [[fn('COUNT', col('url')), 'imageCount']],
+		},
+	});
+	if (!review) {
 		return res.status(404).json({
 			message: `Review couldn't be found`,
 		});
+	}
+	if (review.imageCount >= 10) {
+		return res.status(403).json({
+			message: 'Maximum number of iamges for this resource was reached',
+		});
+	}
 	const img = await ReviewImage.create({
 		reviewId: review.id,
 		url: url,
 	});
-	const respo = ReviewImages.findByPk(img.id, {
-		attributes: ['id', 'url'],
-	});
+	const respo = {
+		id: img.id,
+		url: img.url,
+	};
 	res.status(201).json(respo);
 });
 
@@ -67,11 +79,31 @@ router.put('/:reviewId', requireAuth, async (req, res, next) => {
 	const { user } = req;
 	const { rev, stars } = req.body;
 	const review = await Review.findByPk(req.params.reviewId);
-	await review.update({
-		review: rev,
-		stars: stars,
-	});
-	res.status(200).json(review);
+	if (!review) {
+		return res.status(404).json({
+			message: `Review couldn't be found`,
+		});
+	}
+	try {
+		await review.update({
+			review: rev,
+			stars: stars,
+		});
+		res.status(200).json(review);
+	} catch (error) {
+		console.error(error);
+		if (error instanceof ValidationError) {
+			const errors = {};
+			error.errors.forEach((err) => {
+				errors[err.path] = err.message;
+			});
+			return res.status(400).json({
+				message: 'Bad Request',
+				errors,
+			});
+		}
+		return res.status(500).json({ message: 'Internal Server Error' });
+	}
 });
 
 // DELETE a review by id
@@ -79,6 +111,11 @@ router.put('/:reviewId', requireAuth, async (req, res, next) => {
 router.delete('/:reviewId', requireAuth, async (req, res, next) => {
 	const { user } = req;
 	const review = await Review.findByPk(req.params.reviewId);
+	if (!review) {
+		res.status(404).json({
+			message: `Review couldn't be found`,
+		});
+	}
 	await review.destroy;
 	res.status(200).json({
 		message: 'Successfully deleted',
