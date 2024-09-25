bdc14673acafc847fecd8f989ed3bcaf1c32b09a
edwbartels <edwbartels@gmail.com>
2024-09-25 19:24:55 -0400

    migration debugging

--- backend/db/migrations/20240918204348-create-user.js
+++ backend/db/migrations/20240918204348-create-user.js
@@ -51,7 +51,6 @@ module.exports = {
 		);
 	},
 	async down(queryInterface, Sequelize) {
-		options.tableName = 'Users';
-		await queryInterface.dropTable(options);
+		await queryInterface.dropTable('Users');
 	},
 };
