239efd853530af9d89edf317cec7fbe4c109a1ff
edwbartels <edwbartels@gmail.com>
2024-09-25 19:50:41 -0400

    .

--- backend/db/migrations/20240923153734-create-spot.js
+++ backend/db/migrations/20240923153734-create-spot.js
@@ -16,7 +16,7 @@ module.exports = {
 			ownerId: {
 				allowNull: false,
 				type: Sequelize.INTEGER,
-				references: { model: 'Users', key: 'id' },
+				references: { model: { tableName: 'Users' }, key: 'id' },
 				onDelete: 'CASCADE',
 			},
 			address: {
