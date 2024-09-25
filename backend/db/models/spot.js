7b8756dbfbdfb1f38a1e42103a157d6407fe9d47
edwbartels <edwbartels@gmail.com>
2024-09-24 20:21:23 -0400

    spot validations

--- backend/db/models/spot.js
+++ backend/db/models/spot.js
@@ -24,39 +24,88 @@ module.exports = (sequelize, DataTypes) => {
 				type: DataTypes.STRING,
 				allowNull: false,
 				unique: true,
+				validate: {
+					notEmpty: {
+						msg: `Street address is required`,
+					},
+				},
 			},
 			city: {
 				type: DataTypes.STRING,
 				allowNull: false,
+				validate: {
+					notEmpty: {
+						msg: `City is required`,
+					},
+				},
 			},
 			state: {
 				type: DataTypes.STRING,
 				allowNull: false,
+				validate: {
+					notEmpty: {
+						msg: `State is required`,
+					},
+				},
 			},
 			country: {
 				type: DataTypes.STRING,
 				allowNull: false,
+				validate: {
+					notEmpty: {
+						msg: `Country is required`,
+					},
+				},
 			},
 			lat: {
 				type: DataTypes.DECIMAL,
 				allowNull: false,
+				validate: {
+					len: {
+						args: [-90, 90],
+						msg: `Latitude must be within -90 and 90`,
+					},
+				},
 			},
 			lng: {
 				type: DataTypes.DECIMAL,
 				allowNull: false,
+				validate: {
+					len: {
+						args: [-180, 180],
+						msg: `Longitude must be within -180 and 180`,
+					},
+				},
 			},
 			name: {
 				type: DataTypes.STRING,
 				allowNull: false,
 				unique: true,
+				validate: {
+					max: {
+						args: [50],
+						msg: `Name must be less than 50 characters`,
+					},
+				},
 			},
 			description: {
 				type: DataTypes.STRING,
 				allowNull: false,
+				validate: {
+					notEmpty: {
+						msg: ` Description is required`,
+					},
+				},
 			},
 			price: {
 				type: DataTypes.DECIMAL,
 				allowNull: false,
+				validate: {
+					min: {
+						args: [0.01],
+						msg: `Price per day must be a positive number`,
+					},
+				},
 			},
 		},
 		{
