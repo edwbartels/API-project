43adc4e0a774ef5498e27e32e1a9f82f2ed0df98
edwbartels <edwbartels@gmail.com>
2024-09-25 20:16:35 -0400

    ;.

--- backend/db/migrations/20240923153734-create-spot.js
+++ backend/db/migrations/20240923153734-create-spot.js
@@ -6,71 +6,75 @@ if (process.env.NODE_ENV === 'production') {
 }
 module.exports = {
 	async up(queryInterface, Sequelize) {
-		await queryInterface.createTable('Spots', {
-			id: {
-				allowNull: false,
-				autoIncrement: true,
-				primaryKey: true,
-				type: Sequelize.INTEGER,
-			},
-			ownerId: {
-				allowNull: false,
-				type: Sequelize.INTEGER,
-				references: {
-					model: { tableName: 'Users', schema: process.env.SCHEMA },
-					key: 'id',
+		await queryInterface.createTable(
+			'Spots',
+			{
+				id: {
+					allowNull: false,
+					autoIncrement: true,
+					primaryKey: true,
+					type: Sequelize.INTEGER,
 				},
-				onDelete: 'CASCADE',
+				ownerId: {
+					allowNull: false,
+					type: Sequelize.INTEGER,
+					references: {
+						model: { tableName: 'Users', schema: process.env.SCHEMA },
+						key: 'id',
+					},
+					onDelete: 'CASCADE',
+				},
+				address: {
+					allowNull: false,
+					type: Sequelize.STRING,
+					unique: true,
+				},
+				city: {
+					allowNull: false,
+					type: Sequelize.STRING,
+				},
+				state: {
+					allowNull: false,
+					type: Sequelize.STRING,
+				},
+				country: {
+					allowNull: false,
+					type: Sequelize.STRING,
+				},
+				lat: {
+					allowNull: false,
+					type: Sequelize.DECIMAL,
+				},
+				lng: {
+					allowNull: false,
+					type: Sequelize.DECIMAL,
+				},
+				name: {
+					allowNull: false,
+					type: Sequelize.STRING,
+					unique: true,
+				},
+				description: {
+					allowNull: false,
+					type: Sequelize.STRING,
+				},
+				price: {
+					allowNull: false,
+					type: Sequelize.DECIMAL,
+				},
+				createdAt: {
+					allowNull: false,
+					type: Sequelize.DATE,
+					defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+				},
+				updatedAt: {
+					allowNull: false,
+					type: Sequelize.DATE,
+					defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+				},
 			},
-			address: {
-				allowNull: false,
-				type: Sequelize.STRING,
-				unique: true,
-			},
-			city: {
-				allowNull: false,
-				type: Sequelize.STRING,
-			},
-			state: {
-				allowNull: false,
-				type: Sequelize.STRING,
-			},
-			country: {
-				allowNull: false,
-				type: Sequelize.STRING,
-			},
-			lat: {
-				allowNull: false,
-				type: Sequelize.DECIMAL,
-			},
-			lng: {
-				allowNull: false,
-				type: Sequelize.DECIMAL,
-			},
-			name: {
-				allowNull: false,
-				type: Sequelize.STRING,
-				unique: true,
-			},
-			description: {
-				allowNull: false,
-				type: Sequelize.STRING,
-			},
-			price: {
-				allowNull: false,
-				type: Sequelize.DECIMAL,
-			},
-			createdAt: {
-				allowNull: false,
-				type: Sequelize.DATE,
-				defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
-			},
-			updatedAt: {
-				allowNull: false,
-				type: Sequelize.DATE,
-				defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
-			},
-		});
+			options
+		);
 	},
 	async down(queryInterface, Sequelize) {
 		options.tableName = 'Spots';
